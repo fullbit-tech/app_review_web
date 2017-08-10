1d594520014e093e12177742dfd9988bf106115b
Colin Morgan <colin.morgan@fullbit.ca>
2017-08-10 17:57:38 -0230

    Fix recipe action urls

--- src/actions/recipe.js
+++ src/actions/recipe.js
@@ -15,13 +15,13 @@ const getConfig  = (token) => {
 };
 
 const getRecipes = (token) => axios.get(
-    API_ROOT + '/recipe/', getConfig(token))
+    API_ROOT + '/recipe', getConfig(token))
 
 const getRecipe = (recipeId, token) => axios.get(
     API_ROOT + '/recipe/' + recipeId, getConfig(token));
 
 const createRecipe = (token, name, script) => axios.post(
-    API_ROOT + '/recipe/',
+    API_ROOT + '/recipe',
     {name: name, script: script}, getConfig(token));
 
 const editRecipe = (recipeId, token, name, script) => axios.put(
